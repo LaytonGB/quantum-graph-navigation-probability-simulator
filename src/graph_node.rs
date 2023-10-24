7117dd76248777b95b688b57a78a0b1cef375997
Layton Burchell <laytonb@hotmail.co.uk>
2023-10-24 13:41:03 +0100

    add impls for existing plot structs into graph node

--- src/graph_node.rs
+++ src/graph_node.rs
@@ -1,3 +1,6 @@
+use egui::Pos2;
+use egui_plot::PlotPoint;
+
 #[derive(Copy, Clone, PartialEq, PartialOrd, Debug, serde::Serialize, serde::Deserialize)]
 pub struct GraphNode {
     pub x: f64,
@@ -25,6 +28,20 @@ impl Into<GraphNode> for (f64, f64) {
             x: self.0,
             y: self.1,
         }
+    }
+}
+
+impl Into<GraphNode> for PlotPoint {
+    fn into(self) -> GraphNode {
+        let PlotPoint { x, y } = self;
+        GraphNode { x, y }
+    }
+}
+
+impl Into<GraphNode> for Pos2 {
+    fn into(self) -> GraphNode {
+        let Pos2 { x, y } = self;
+        GraphNode { x: x as f64, y: y as f64 }
     }
 }
 
