52166223b66f368a7cd35e6b2cf61efce05a877f
Layton Burchell <laytonb@hotmail.co.uk>
2023-10-31 10:22:07 +0000

    remove completed todo

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-// TODO right-click -> Add Node by coordinates
 // TODO clean all imports/exports
 // TODO add "add graph" setting under canvas (place graph without clearing
 // canvas)
