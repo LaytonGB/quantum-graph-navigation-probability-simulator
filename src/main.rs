a6e30cbd03a96a08a1d69b9c38f89cd4cc42d789
Layton <laytonb@hotmail.co.uk>
2024-08-08 23:58:11 +0100

    add build flag (+ cleanup)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-#![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-// When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result<()> {
     std::env::set_var("RUST_BACKTRACE", "1"); // show backtrace in case of crash
-
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
     let native_options = eframe::NativeOptions {
@@ -19,7 +16,6 @@ fn main() -> eframe::Result<()> {
     )
 }
 
-// When compiling to web using trunk:
 #[cfg(target_arch = "wasm32")]
 fn main() {
     // Redirect `log` message to `console.log` and friends:
